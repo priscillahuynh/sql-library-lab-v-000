671c52791e3e958acaebc59c204a39cd16d5403e
Priscilla H <priscillamhuynh@gmail.com>
2018-12-23 06:05:48 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -9,3 +9,24 @@ CREATE TABLE Sub_genre (
   id INTEGER PRIMARY KEY,
   name TEXT,
 );
+
+CREATE TABLE Authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+);
+
+CREATE TABLE Books (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER
+);
+
+CREATE TABLE Characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id INTEGER,
+  series_id INTEGER
+);
