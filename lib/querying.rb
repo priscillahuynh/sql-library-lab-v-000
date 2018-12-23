e469ac6ae8a2d759f2418eb4842ebbad22bbcd9d
Priscilla H <priscillamhuynh@gmail.com>
2018-12-23 07:27:22 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,8 +25,9 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT title FROM series WHERE species == "human"
-   INNER JOIN characters ON characters.series_id = series.id;"
+  "SELECT title FROM series 
+   INNER JOIN characters ON characters.series_id = series.id
+   WHERE species == "human";"
 end
 
 def select_character_names_and_number_of_books_they_are_in
