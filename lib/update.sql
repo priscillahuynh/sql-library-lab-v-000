5fdd49d3545c40b972a947d558a5985d1ec43dd8
Priscilla H <priscillamhuynh@gmail.com>
2018-12-23 06:40:56 +0000

    Automatically backed up by Learn

--- lib/update.sql
+++ lib/update.sql
@@ -1,1 +1,1 @@
-UPDATE characters SET species = "Martian" WHERE species  = characters.species ORDER BY species DESC limit 1;
+UPDATE characters SET species = "Martian" WHERE species == characters.species ORDER BY species DESC limit 1;
