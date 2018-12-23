e5fea2b56e22ecd630a62c342793d81daf511c1b
Priscilla H <priscillamhuynh@gmail.com>
2018-12-23 07:26:45 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,7 +25,10 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT title FROM series WHERE species = "human"
+   INNER JOIN characters ON characters.series_id = series.id
+   GROUP BY species
+   ORDER BY "
 end
 
 def select_character_names_and_number_of_books_they_are_in
