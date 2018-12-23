a98153caccaf9e9f13088d02347fd1427248d251
Priscilla H <priscillamhuynh@gmail.com>
2018-12-23 07:23:02 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,7 +20,8 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM authors
-   INNER JOIN subgenres ON authors.id = series.author_id;"
+   INNER JOIN subgenres ON subgenres.id = series.subgenre_id
+   INNER JOIN series ON series.author_id = authors.id;"
 end
 
 def select_series_title_with_most_human_characters
