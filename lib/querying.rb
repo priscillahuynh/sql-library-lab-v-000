01f2885140bd822a8a02a6daac233c4aea39d75d
Priscilla H <priscillamhuynh@gmail.com>
2018-12-23 07:11:13 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,7 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(species) AS count_species FROM characters
    GROUP BY species
-   ORDER BY SUM(species) DESC LIMIT 1;"
+   ORDER BY count_species) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
