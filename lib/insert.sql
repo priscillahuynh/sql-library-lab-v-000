04717b54357ea47be30acaba7a77b63ffce3c1b7
Priscilla H <priscillamhuynh@gmail.com>
2018-12-23 06:25:25 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO series (title, author_id, subgenre_id) VALUES ("A Song of Ice and Fi
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("Second Series", 2, 2);
 
 INSERT INTO books (title, year, series_id) VALUES ("Game of Thrones", 1996, 1), ("A Clash of Kings", 1998, 1), ("A Storm of Swords", 2000, 1), ("First Book", 2002, 2), ("Second Book", 2003, 2), ("Third Book", 2005, 2);
+
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES 
