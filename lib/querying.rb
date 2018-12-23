11138353efb910fb014db2da995074d60933aaba
Priscilla H <priscillamhuynh@gmail.com>
2018-12-23 07:33:40 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(character_books.book_id) AS count FROM characters
+  "SELECT characters.name, COUNT(character_books.book_id) AS count FROM character_books
    INNER JOIN character_books ON character_id = characters.id
    ORDER BY count DESC LIMIT 1;"
 end
