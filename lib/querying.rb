e9479026ea2ba0ce3e465aace8dca10498beca28
Priscilla H <priscillamhuynh@gmail.com>
2018-12-23 07:16:23 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, series.subgenre_id FROM authors
+  "SELECT authors.name, subgenres.name FROM authors
    INNER JOIN series ON authors.id = series.author_id;"
 end
 
